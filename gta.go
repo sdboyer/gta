3278192af72d2128f2e90ecd0b24981c5bcc3789
sam boyer <tech@samboyer.org>
2016-08-30 22:09:43 -0400

    Rejigger some more, getting fairly close

--- gta.go
+++ gta.go
@@ -73,11 +73,12 @@ func RunGTA(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("Could not get working directory: %s", err)
 	}
 
-	sm, err := gps.NewSourceManager(dependency.Analyzer{}, filepath.Join(gpath.Home(), "cache"), false)
-	defer sm.Release()
+	an := dependency.Analyzer{}
+	sm, err := gps.NewSourceManager(an, filepath.Join(gpath.Home(), "cache"), false)
 	if err != nil {
 		return fmt.Errorf("Failed to set up SourceManager: %s", err)
 	}
+	defer sm.Release()
 
 	root, err := sm.DeduceProjectRoot(pkg)
 	if err != nil {
@@ -96,6 +97,8 @@ func RunGTA(cmd *cobra.Command, args []string) error {
 		// shouldn't be possible, but whatever
 		return fmt.Errorf("No versions could be located for %s", pi)
 	}
+
+	gps.SortForUpgrade(vlist)
 
 	// obnoxious constraint parsing
 	var c gps.Constraint
@@ -127,15 +130,38 @@ func RunGTA(cmd *cobra.Command, args []string) error {
 	srcprefix := filepath.Join(build.Default.GOPATH, "src") + string(filepath.Separator)
 	importroot := filepath.ToSlash(strings.TrimPrefix(wd, srcprefix))
 
+	// Use the analyzer to figure out this project, too
+	m, l, err := an.DeriveManifestAndLock(wd, gps.ProjectRoot(importroot))
+	if err != nil {
+		return fmt.Errorf("Error on trying to read project manifest and lock: %s", err)
+	}
+	rm := prepManifest(m)
+
+	var focus gps.ProjectConstraint
+	var has bool
+	if focus, has = rm.c[root]; !has {
+		focus = gps.ProjectConstraint{
+			Ident: gps.ProjectIdentifier{
+				ProjectRoot: root,
+			},
+		}
+	}
+
+	focus.Constraint = c
+
 	// Set up params, including tracing
 	params := gps.SolveParameters{
+		Manifest:   rm,
+		Lock:       l,
 		RootDir:    wd,
 		ImportRoot: gps.ProjectRoot(importroot),
+		//Trace:       true,
+		//TraceLogger: log.New(os.Stdout, "", 0),
 	}
 
 	var vl []gps.Version
 	for _, v := range vlist {
-		if c.Matches(v) {
+		if focus.Constraint.Matches(v) {
 			vl = append(vl, v)
 		}
 	}
@@ -144,7 +170,7 @@ func RunGTA(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("%s has %v versions, but none matched constraint %s", root, len(vlist), c)
 	}
 
-	fmt.Println("Checking %s with the following versions: %s", root, vl)
+	fmt.Printf("Checking %s with the following versions:\n\t%s\n", root, vl)
 
 	type solnOrErr struct {
 		v   gps.Version
@@ -154,7 +180,10 @@ func RunGTA(cmd *cobra.Command, args []string) error {
 
 	solns := make([]solnOrErr, len(vlist))
 	for k, v := range vlist {
-		// TODO assign v into manifest
+		fmt.Printf("Looking for solution with %s@%s...", root, v)
+		focus.Constraint = v
+		rm.c[root] = focus
+
 		// TODO parallel, bwahaha
 		soe := solnOrErr{v: v}
 		// TODO reparse root project every time...horribly wasteful
@@ -162,11 +191,16 @@ func RunGTA(cmd *cobra.Command, args []string) error {
 		s, soe.err = gps.Prepare(params, sm)
 		if soe.err == nil {
 			soe.s, soe.err = s.Solve()
-			continue
 		}
 
+		if soe.err == nil {
+			fmt.Println("done.")
+		} else {
+			fmt.Println("failed.")
+		}
 		solns[k] = soe
 	}
+	fmt.Println("") // spacer
 
 	// If we have to create these vendor trees, then back up the original vendor
 	vpath := filepath.Join(string(root), "vendor")
@@ -178,43 +212,95 @@ func RunGTA(cmd *cobra.Command, args []string) error {
 			}
 			defer os.Rename(filepath.Join(string(root), "_origvendor"), vpath)
 		}
-	}
 
-	var fail bool
-	for _, soln := range solns {
-		nv := fmt.Sprintf("%s@%s", root, soln.v)
-		if soln.err != nil {
-			fail = true
-			fmt.Printf("%s failed solving: %s\n", nv, soln.err)
-			continue
-		}
-
-		if run != "" {
-			fmt.Printf("%s succeeded", nv)
-		} else {
-			err = gps.WriteDepTree(vpath, soln.s, sm, true)
-			if err != nil {
+		var fail bool
+		for _, soln := range solns {
+			nv := fmt.Sprintf("%s@%s", root, soln.v)
+			if soln.err != nil {
 				fail = true
-				fmt.Printf("could not write tree for %s, skipping check", nv)
+				fmt.Printf("%s failed solving: %s\n", nv, soln.err)
 				continue
 			}
 
-			parts := strings.Split(run, " ")
-			cmd := exec.Command(parts[0], parts[1:]...)
-			out, err := cmd.CombinedOutput()
-			if err != nil {
-				fail = true
-				fmt.Printf("%s failed with %s, output:\n%s", err, string(out))
+			if run != "" {
+				fmt.Printf("%s succeeded\n", nv)
 			} else {
-				fmt.Printf("%s succeeded", nv)
-			}
+				err = gps.WriteDepTree(vpath, soln.s, sm, true)
+				if err != nil {
+					fail = true
+					fmt.Printf("skipping check: could not write tree for %s (err %s)\n", nv, err)
+					continue
+				}
 
-			os.RemoveAll(vpath)
+				parts := strings.Split(run, " ")
+				cmd := exec.Command(parts[0], parts[1:]...)
+				out, err := cmd.CombinedOutput()
+				if err != nil {
+					fail = true
+					fmt.Printf("%s failed with %s, output:\n%s\n", err, string(out))
+				} else {
+					fmt.Printf("%s succeeded\n", nv)
+				}
+
+				os.RemoveAll(vpath)
+			}
+		}
+
+		if fail {
+			return fmt.Errorf("Encountered one or more errors")
 		}
 	}
 
-	if fail {
-		return fmt.Errorf("Encountered one or more errors")
-	}
 	return nil
+}
+
+type simpleRootManifest struct {
+	c   map[gps.ProjectRoot]gps.ProjectConstraint
+	tc  map[gps.ProjectRoot]gps.ProjectConstraint
+	ovr gps.ProjectConstraints
+	ig  map[string]bool
+}
+
+func (m simpleRootManifest) DependencyConstraints() []gps.ProjectConstraint {
+	ds := make([]gps.ProjectConstraint, 0)
+	for _, d := range m.c {
+		ds = append(ds, d)
+	}
+	return ds
+}
+
+func (m simpleRootManifest) TestDependencyConstraints() []gps.ProjectConstraint {
+	ds := make([]gps.ProjectConstraint, 0)
+	for _, d := range m.tc {
+		ds = append(ds, d)
+	}
+	return ds
+}
+
+func (m simpleRootManifest) Overrides() gps.ProjectConstraints {
+	return m.ovr
+}
+
+func (m simpleRootManifest) IgnorePackages() map[string]bool {
+	return m.ig
+}
+
+func prepManifest(m gps.Manifest) simpleRootManifest {
+	rm := simpleRootManifest{
+		c:  make(map[gps.ProjectRoot]gps.ProjectConstraint),
+		tc: make(map[gps.ProjectRoot]gps.ProjectConstraint),
+	}
+
+	if m == nil {
+		return rm
+	}
+
+	for _, d := range m.DependencyConstraints() {
+		rm.c[d.Ident.ProjectRoot] = d
+	}
+	for _, d := range m.TestDependencyConstraints() {
+		rm.tc[d.Ident.ProjectRoot] = d
+	}
+
+	return rm
 }
